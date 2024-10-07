1b80e5bffe8b06bfef8d2216513e4496cf13621f
Rodrigo Fernandes da Costa <rodrigo5244@gmail.com>
2024-10-07 03:36:37 +0100

    Add workers and logs APIs to AWS CDK stack

--- infra/src/BotsStack.ts
+++ infra/src/BotsStack.ts
@@ -11,19 +11,45 @@ export class BotsStack extends cdk.Stack {
 
     const region = this.region;
 
-    const { apiUrl, getBotsLambda, addBotLambda, updateBotLambda } = makeApis(this, region);
+    const {
+      apiUrl,
+      getBotsLambda,
+      addBotLambda,
+      updateBotLambda,
+      addLogLambda,
+      addWorkerLambda,
+      getLogsLambda,
+      getWorkersLambda,
+      updateLogLambda,
+      updateWorkerLambda,
+    } = makeApis(this, region);
 
     const botsTable = new dynamodb.Table(this, "botsTable", {
-      partitionKey: {
-        name: "botId",
-        type: dynamodb.AttributeType.STRING,
-      },
+      partitionKey: { name: "botId", type: dynamodb.AttributeType.STRING },
       tableName: "botsTable",
+    });
+
+    const workersTable = new dynamodb.Table(this, "workersTable", {
+      partitionKey: { name: "workerId", type: dynamodb.AttributeType.STRING },
+      tableName: "workersTable",
+    });
+
+    const logsTable = new dynamodb.Table(this, "logsTable", {
+      partitionKey: { name: "logId", type: dynamodb.AttributeType.STRING },
+      tableName: "logsTable",
     });
 
     botsTable.grantReadWriteData(getBotsLambda);
     botsTable.grantReadWriteData(addBotLambda);
     botsTable.grantReadWriteData(updateBotLambda);
+
+    workersTable.grantReadWriteData(getWorkersLambda);
+    workersTable.grantReadWriteData(addWorkerLambda);
+    workersTable.grantReadWriteData(updateWorkerLambda);
+
+    logsTable.grantReadWriteData(getLogsLambda);
+    logsTable.grantReadWriteData(addLogLambda);
+    logsTable.grantReadWriteData(updateLogLambda);
 
     makeCdn(this, apiUrl);
   }
--- infra/src/makeApis.ts
+++ infra/src/makeApis.ts
@@ -1,5 +1,15 @@
 import { HttpApi } from "aws-cdk-lib/aws-apigatewayv2";
-import { addBotApi, getBotsApi, updateBotApi } from "backend";
+import {
+  addBotApi,
+  addLogApi,
+  addWorkerApi,
+  getBotsApi,
+  getLogsApi,
+  getWorkersApi,
+  updateBotApi,
+  updateLogApi,
+  updateWorkerApi,
+} from "backend";
 import { Construct } from "constructs";
 
 import { makeApi } from "./makeApi.js";
@@ -15,8 +25,17 @@ export function makeApis(scope: Construct, region: string) {
   return {
     api,
     apiUrl: `${api.apiId}.execute-api.${region}.amazonaws.com`,
+
     getBotsLambda: makeApi({ api, region, scope, ...getBotsApi }),
+    getWorkersLambda: makeApi({ api, region, scope, ...getWorkersApi }),
+    getLogsLambda: makeApi({ api, region, scope, ...getLogsApi }),
+
     addBotLambda: makeApi({ api, region, scope, ...addBotApi }),
+    addWorkerLambda: makeApi({ api, region, scope, ...addWorkerApi }),
+    addLogLambda: makeApi({ api, region, scope, ...addLogApi }),
+
     updateBotLambda: makeApi({ api, region, scope, ...updateBotApi }),
+    updateWorkerLambda: makeApi({ api, region, scope, ...updateWorkerApi }),
+    updateLogLambda: makeApi({ api, region, scope, ...updateLogApi }),
   };
 }
